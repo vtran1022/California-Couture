fff609b6f0cf6c7b60b14281f484be9611861991
jsiegelman1 <jacob.siegelman1@gmail.com>
2021-06-04 15:57:17 -0700

    Added basic API wrapper

---
+++ client/src/components/RatingsReview.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Ratings = (props) => {
+  return <div>Ratings and Reviews</div>
+};
+export default Ratings;
--- config.example.js
+++ config.example.js
@@ -1,3 +1,3 @@
-export default = {
-  TOKEN: 'YOUR TOKEN HERE'
+export var auth = {
+  TOKEN: ''
 };
