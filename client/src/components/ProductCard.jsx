91bf5833a257c0066749fd5ce5796a631942622c
Vy Tran <vtran1022@Gmail.com>
2021-06-07 15:28:09 -0700

    Created product card base

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { auth } from '../../../config.js';
+
+// take in destructing {productId}
+function ProductCard() {
+  const productId = 13027; //placeholder for testing
+
+  async function fetchProducts() {
+    let productData = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/products/${productId}`,
+    { headers: { 'Authorization': auth.TOKEN } })
+
+    console.log(productData.data);
+  };
+
+  useEffect(() => {
+    fetchProducts().catch((err) => console.log(`Error fetching product info: ${err}`))
+  }, []);
+
+  return (
+    <div>
+      <h1>Testing-ProductCard</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default ProductCard;
+
+/*
+
+- pull productId from relatedList
+
+1) product preview image
+2) product category
+3) product price
+4) star rating
+
+*/
