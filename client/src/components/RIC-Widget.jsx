dda0cc7285ecfcad1017c2aa8e45bd255f7e884a
Vy Tran <vtran1022@Gmail.com>
2021-06-07 22:24:10 -0700

    Added code to widget component to render both lists

--- client/src/components/RIC-Widget.jsx
+++ client/src/components/RIC-Widget.jsx
@@ -1,2 +1,17 @@
 import React from 'react';
-import
+import RelatedList from './RIC-Widget/RelatedList.jsx';
+import OutfitList from './RIC-Widget/OutfitList.jsx';
+
+// expecting this productId to be linked to current Overview Product
+function RICWidget({ productId }) {
+  return (
+    <div>
+      <RelatedList
+        productId={productId}/>
+      <OutfitList
+        productId={productId}/>
+    </div>
+  );
+};
+
+export default RICWidget;
