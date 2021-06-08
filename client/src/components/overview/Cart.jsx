ba49547892345606dde1026f8ed77d6ed2d5c569
Julian Thong <julianzthong@gmail.com>
2021-06-07 20:31:04 -0700

    Components will render asynchronously now. Need to figure out logic to get the dropdown menus for sizes and quantities to work properly. clicking on the style will change the state of the current selected style.

--- client/src/components/overview/Cart.jsx
+++ client/src/components/overview/Cart.jsx
@@ -1,22 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Cart ( { styleList, defaultPrice } ) {
+const Cart = ( props ) => {
   // const [cartItems, serCurrentCart] = await useState({}) need to pull a user's cart
-  const [size, setSize] = useState({})
+
+  const [styleId, setStyleId] = useState('');
+  const [selectedStyle, setStyle] = useState(props.styleList);
+  const [sizes, setSizes] = useState ([]);
+  const [quantity, setQuantities] = useState ([]);
+  const [selectedSize, setSize] = useState('');
+  const [selectedQuantity, setQuantity] = useState('');
+
+  useEffect( () => {
+    setStyle(props.styleList);
+  }, [])
+
+  function handleStyleClick(value) {
+    // iterate over the stylesList.
+    // if the stylesList contains the value as a value
+    // setStyle that style
+    let list = props.styleList;
+    // let listed = list.styleList;
+    for (var i = 0; i < list.length; i++) {
+      if (Object.values(list[i]).includes(Number(value))){
+        setStyle(list[i])
+      }
+    }
+  }
 
   return (
-    <div>Cart
+    <div >Cart
+      <div>
+      {selectedStyle.length
+      ? (props.styleList.map(
+        style => <div
+        key={style.style_id}
+        onClick={(e) => handleStyleClick(e.target.getAttribute('key'))}>
+          {style.name}
+        </div>))
+      : <p>No Product Selected</p>
+    }
+      </div>
 
-    <select name='size' id='size-select' onChange={(e) => setStyle(e.target.value)}>
+    <select name='size' id='size-select' onChange={(e) => setSize(e.target.value)}>
       <option>Select Size</option>
-      { styleList.map( style =>
-        <option key={style.style_id}>{style.name}</option>
-      )}
+      {
+        selectedStyle.length
+        ? selectedStyle.map( entry =>
+          {for (var key in entry.skus) {
+             <option>
+              {entry.skus[key].size}
+            </option>
+          }} )
+          : null
+        }
+    </select>
+
+    <select name='quantity' id='quantity-select' onChange={(e) => setQuantity(e.target.value)}>
+      <option></option>
     </select>
       </div>
   )
 
-}
 
+
+}
+// function parseSKUs (style) {
+  //   let arrayOfSizesAndQuantities = []
+  //   console.log(style)
+  //   for (var key in style.style) {
+    //     console.log(key)
+    //     console.log(style.style)
+    //     arrayOfSizesAndQuantities.push({
+      //       skuID: key,
+      //       size: style.style[key]
+      //     })
+      //   }
+      //   console.log(arrayOfSizesAndQuantities)
+// }
 
 export default Cart;
--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import ImageGallery from './ImageGallery.jsx';
 import Cart from './Cart.jsx';
 import Atelier from '/Users/julianzthong/Desktop/Hack_Reactor/FEC/client/src/Atelier.js';
+import { auth } from '/Users/julianzthong/Desktop/Hack_Reactor/FEC/config.js';
 
 /*
 Need to get ONE product from the database
@@ -12,15 +14,25 @@ Styles also contains all the inventory of the product styles
 function Overview (props) {
 
   // const [productList, setProductList] = useState(sampleData); don't think this is necessary.
+  const [isLoading, setLoading] = useState(false)
   const [currentProduct, setCurrentProduct] = useState({});
-  const [styles, setStyle] = useState([]);
+  const [styles, setStyleList] = useState([]);
+  const [style, setStyle] = useState({});
 
   useEffect( () => {
-    Atelier.getInfo(13023)
-    .then(data => setCurrentProduct(data))
-    Atelier.getStyles(13023)
-    .then(data => setStyle(data.results))
-  }, [])
+    async function fetchData () {
+      setLoading(true)
+      await Atelier.getInfo(13023)
+      .then(data => setCurrentProduct(data));
+      await Atelier.getStyles(13023)
+      .then(data => {
+        setStyleList(data.results);
+        return data})
+        .then(data => setStyle(data.results[0].skus))
+      setLoading(false)
+      }
+    fetchData()
+    }, [])
 
 
   return (
@@ -35,6 +47,7 @@ function Overview (props) {
       <span className='category'>
         { currentProduct.category }
         </span>
+        <br></br>
       <span className='name'>
         { currentProduct.name }
         </span>
@@ -42,13 +55,15 @@ function Overview (props) {
         {/* {if (currentProduct.description) {
           <p className='product-info'}>{currentProduct.description}</p>
         } */}
-
-        <Cart
+        {isLoading
+        ? <div>Loading</div>
+        : <Cart
         key={ currentProduct.id }
         styleList={ styles }
+        style={ style }
         defaultPrice={ currentProduct.default_price }
         />
-
+      }
       <button>
         Facebook
       </button>
