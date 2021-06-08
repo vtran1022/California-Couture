9832caf095b2c266a35c260b692356ce5ad04117
Vy Tran <vtran1022@Gmail.com>
2021-06-07 21:55:19 -0700

    App file is used for testing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import axios from 'axios';
-import Atelier from '../Atelier.js';
-import Ratings from './RatingsReview.jsx';
-import AvgRating from './AvgRating.jsx';
+import OutfitList from './RIC Widget/OutfitList.jsx';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      holder: ''
+      product: 13030,
+      idArr: [
+        13027,
+        13031,
+        13029,
+        13024,
+        13023
+      ]
     }
+
+    this.handleItem = this.handleItem.bind(this);
+  }
+
+  handleItem(event) {
+    this.setState({product: event.target.innerText});
   }
 
   render() {
     return (
       <div>
-        <h1>Holder</h1>
-        <AvgRating />
+        {this.state.idArr.map((item) => (
+          <div key={item} onClick={this.handleItem}>{item}</div>
+        ))}
+        <div>
+          <OutfitList
+            productId={this.state.product}/>
+        </div>
       </div>
     );
   }
