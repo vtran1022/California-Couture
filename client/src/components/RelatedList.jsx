54fb1fd014a345cf95d17d0d120ac70d227be775
Vy Tran <vtran1022@Gmail.com>
2021-06-07 18:44:36 -0700

    MVP for related item list completed, a list is rendering of related items e

--- client/src/components/RelatedList.jsx
+++ client/src/components/RelatedList.jsx
@@ -1,3 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { auth } from '../../../config.js';
+import ProductCard from './ProductCard.jsx';
+
+// take in destructing { productId } from overview design product
+function RelatedList() {
+  const productId = 13027; //placeholder for testing
+  const [relatedItems, setRelated] = useState([]);
+
+  async function fetchRelated() {
+    let relatedData = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/products/${productId}/related`,
+    { headers: { 'Authorization': auth.TOKEN } });
+
+    setRelated(relatedData.data);
+  };
+
+  useEffect(() => {
+    fetchRelated().catch((err) => console.log(`Error fetching product info: ${err}`))
+  }, [productId]);
+
+  return (
+    <div>
+      <h1>Testing-RelatedList</h1>
+      <div>
+        {relatedItems.map((id, index) => (
+          <ProductCard
+            productId={id} key={index}/>))}
+      </div>
+    </div>
+  );
+
+};
+
+export default RelatedList;
+
+
 /*
 - get product Id from overview design product to then map over that list
 
