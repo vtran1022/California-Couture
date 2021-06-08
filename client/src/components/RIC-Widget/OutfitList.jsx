e323f831c72239fb125d445c534a514b8f837b7d
Vy Tran <vtran1022@Gmail.com>
2021-06-07 22:18:43 -0700

    Added React back into import statement

--- client/src/components/RIC-Widget/OutfitList.jsx
+++ client/src/components/RIC-Widget/OutfitList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ProductCard from './ProductCard.jsx';
 
 // expecting this productId to be linked to current Overview Product
