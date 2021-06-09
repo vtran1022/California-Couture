d9de43fdd206e3dc1693c3acd714c0053d42de87
Vy Tran <vtran1022@Gmail.com>
2021-06-08 22:36:27 -0700

    CLeaning up

--- vy.test.js
+++ vy.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
 import App from './client/src/components/App.jsx';
 
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
+
+    screen.debug();
   });
 });
