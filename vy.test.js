593e9c7a5e0e169167a0a514bdeb9228e0d6d8ce
Vy Tran <vtran1022@Gmail.com>
2021-06-09 13:33:59 -0700

    adding in appropriate tests

--- vy.test.js
+++ vy.test.js
@@ -1,43 +1,77 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
-import App from './client/src/components/App.jsx';
 
-// basic render testing, screen.debug will log the HTML output in test terminal
+import App from './client/src/components/App.jsx';
+import ActionButton from './client/src/components/RIC-Widget/ActionButton.jsx';
+import OutfitList from './client/src/components/RIC-Widget/OutfitList.jsx';
+
+// basic render testing, screen.debug() will log the HTML output in test terminal
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
-
-    screen.debug();
   });
 });
 
-/*
-'getByText' is a search func that 'grabs' a text within the HTML
-- accepts a string as input or a regular expression: screen.getByText(/Outfit/) - this is used for a partial match
-*/
-describe('App', () => {
-  test('renders App component', () => {
-    render(<App />);
+describe('Add to Outfit card showing up', () => {
+  test('check for outfit text', () => {
+    render(<OutfitList />);
 
     expect(screen.getByText('Add to Outfit')).toBeInTheDocument();
   });
 });
 
-/*
-'getByRole' - used to retrieve elements by attributes or by their implicit roles, like a button.
-- a feature is that it will suggest roles if you provide a role that is not available
-*/
+describe('Action Button', () => {
+  test('star action button exists on initial render', () => {
+    render(<ActionButton />);
 
-describe('App', () => {
-  test('renders App component', () => {
-    render(<App />);
-
-    screen.getByRole('button');
+    expect(screen.getByDisplayValue('☆')).toBeInTheDocument();
   });
 });
+
+describe('Action Button', () => {
+  test('delete action button does not exist on initial render', () => {
+    render(<ActionButton />);
+
+    expect(screen.queryByDisplayValue('x')).toBeNull();
+  });
+});
+
+// need to add integration test to test that delete button appears after userClicks
 
 /*
 use 'queryBy' to search for elements that shouldn't be there
 use 'findBy'for asynchronous elements which will be there eventually
 */
+
+/*
+'fireEvent.change' func to simulate interactions of an end user, only change events
+'userEvent.type' func triggers a change event, but also keyDown, keyPress, and keyUp events
+
+Example:
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+
+    // wait for the user to resolve
+    // needs only be used in our special case
+    await screen.findByText(/Signed in as/);
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+
+    OR
+
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  });
+});
+
+*/
+
