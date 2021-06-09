8b4599c032009ea2d8b12bc0b08b825150b086b4
Vy Tran <vtran1022@Gmail.com>
2021-06-09 14:03:32 -0700

    added more test for components

--- vy.test.js
+++ vy.test.js
@@ -6,19 +6,13 @@ import '@testing-library/jest-dom/extend-expect';
 import App from './client/src/components/App.jsx';
 import ActionButton from './client/src/components/RIC-Widget/ActionButton.jsx';
 import OutfitList from './client/src/components/RIC-Widget/OutfitList.jsx';
+import ProductCard from './client/src/components/RIC-Widget/ProductCard.jsx';
+import RelatedList from './client/src/components/RIC-Widget/RelatedList.jsx';
 
 // basic render testing, screen.debug() will log the HTML output in test terminal
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
-  });
-});
-
-describe('Add to Outfit card showing up', () => {
-  test('check for outfit text', () => {
-    render(<OutfitList />);
-
-    expect(screen.getByText('Add to Outfit')).toBeInTheDocument();
   });
 });
 
@@ -38,40 +32,50 @@ describe('Action Button', () => {
   });
 });
 
-// need to add integration test to test that delete button appears after userClicks
+describe('Action Button', () => {
+  test('calls the onClick callback handler', async () => {
+    const onClick = jest.fn();
 
-/*
-use 'queryBy' to search for elements that shouldn't be there
-use 'findBy'for asynchronous elements which will be there eventually
-*/
+    render( <input type="button" value='x' onClick={onClick} /> );
 
-/*
-'fireEvent.change' func to simulate interactions of an end user, only change events
-'userEvent.type' func triggers a change event, but also keyDown, keyPress, and keyUp events
+    await userEvent.click(screen.getByDisplayValue('x'));
 
-Example:
-
-describe('App', () => {
-  test('renders App component', async () => {
-    render(<App />);
-
-    // wait for the user to resolve
-    // needs only be used in our special case
-    await screen.findByText(/Signed in as/);
-
-    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
-
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: 'JavaScript' },
-    });
-
-    OR
-
-    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
-
-    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
 
-*/
+describe('Add to Outfit card showing up', () => {
+  test('check for outfit text', () => {
+    render(<OutfitList />);
 
+    expect(screen.getByText('Add to Outfit')).toBeInTheDocument();
+  });
+});
+
+describe('Outfit List', () => {
+  test('calls the onClick callback handler', async () => {
+    const onClick = jest.fn();
+
+    render( <div className='AddCard' onClick={onClick}>
+      <div data-testid='outfit'>Add to Outfit</div>
+    </div> );
+
+    await userEvent.click(screen.getByTestId('outfit'));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
+
+// checking rendering for now, need to work on better test for this
+describe('Product Card', () => {
+  test('check existance', () => {
+    render(<ProductCard />);
+  });
+});
+
+// checking rendering for now, need to work on better test for this
+describe('Related List', () => {
+  test('check existance', () => {
+    render(<RelatedList />);
+  });
+});
