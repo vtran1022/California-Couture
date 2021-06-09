a9d5b89c366536e8aa89970fee9bedd04bcb3e23
Vy Tran <vtran1022@Gmail.com>
2021-06-08 22:43:07 -0700

    Updated package.json to install jest-dom testing library as well. testing more in vy.test.js

--- vy.test.js
+++ vy.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './client/src/components/App.jsx';
 
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
 
-    screen.debug();
+    // screen.debug();
+    expect(screen.getByText('Add to Outfit')).toBeInTheDocument();
   });
 });
