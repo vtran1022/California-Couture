0ead9c8d53c1b3c257682445f2703cbaeb2fbbc9
Vy Tran <vtran1022@Gmail.com>
2021-06-10 11:46:59 -0700

    adding test to make sure ric-widget is rendering

--- relateditems.test.js
+++ relateditems.test.js
@@ -4,15 +4,23 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from './client/src/components/App.jsx';
+import RICWidget from './client/src/components/RIC-Widget.jsx';
 import ActionButton from './client/src/components/RIC-Widget/ActionButton.jsx';
 import OutfitList from './client/src/components/RIC-Widget/OutfitList.jsx';
 import ProductCard from './client/src/components/RIC-Widget/ProductCard.jsx';
 import RelatedList from './client/src/components/RIC-Widget/RelatedList.jsx';
+import ComparisonModal from './client/src/components/RIC-Widget/ComparisonModal.jsx';
 
 // basic render testing, screen.debug() will log the HTML output in test terminal
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
+  });
+});
+
+describe('RICWidget', () => {
+  test('renders RIC-Widget component - holds both lists', () => {
+    render(<RICWidget />);
   });
 });
 
