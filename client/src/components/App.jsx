2bd8718fbb1fb8975950b370f86e65c9d795b134
Vy Tran <vtran1022@Gmail.com>
2021-06-11 16:00:16 -0700

    Updated product id passed down from app to one w/ long related item list for testing purposes...

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: '13023',
+      product: '13029',
       idArr: [
         13027,
         13031,
