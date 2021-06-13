470d29c8794a7f1d08b02d0d5289d97f60083847
Vy Tran <vtran1022@Gmail.com>
2021-06-13 00:30:56 -0700

    Refactored star component

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -5,30 +5,29 @@ const StarRating = ({ rating }) => {
   var stars = [];
   for(var i = 0; i < 5; i++) {
     if(rating >= 1) {
-      stars.push(1.0);
+      stars.push('100%');
       rating -= 1;
     } else if (rating >= 0.75) {
-      stars.push(0.75)
+      stars.push('75%')
       rating -= 0.75
     } else if (rating >= 0.5) {
-      stars.push(0.5);
+      stars.push('50%');
       rating -= 0.5;
     } else if (rating >= 0.25) {
-      stars.push(0.25);
+      stars.push('25%');
       rating -= 0.25;
     } else {
-      stars.push(0);
+      stars.push('0%');
     }
   }
 
+  console.log(stars);
+
   return (
     <div>
-      <i class="fas fa-star"></i>
       {stars.map((item, i) => (
         <div className='star-container' key={i}>
-            <div className='star-fill' style={{'width': `${parseInt(item*15)}px`}}>
-              <i class="fas fa-star"></i>
-            </div>
+          <i className="fas fa-star" style={{'--width': item }}></i>
         </div>
       ))}
     </div>
