d3a873484bb0a8c22f167edf6995b23f8e89ee42
Vy Tran <vtran1022@Gmail.com>
2021-06-12 18:49:38 -0700

    cleaning up comments

--- client/src/components/RIC-Widget/OutfitList.jsx
+++ client/src/components/RIC-Widget/OutfitList.jsx
@@ -119,22 +119,9 @@ const OutfitList = ({ productId }) => {
 export default OutfitList;
 
 /*
-When clicking 'Add to Outfit', pull the productId of Overview and add productcard
-- on click, add the productid to an outfit array
-- render the product card of that product id
-
-
-
-
-- custom product list created by user w/ products that they've grouped together for 'outfit'
-- similar format to related product cards
 - will not be determined internally but unique to user
  - items only added when explicitly added by user
-- default, no items
-- first card should not contain a product, should have '+' icon and read 'add to outfit'
- - this remains visible even as items are added
 - only unique to specific user, same across the board, regardless where they click to
-- product can only be added once
 - no max # of items to this list
 - PERSIST
  - across page navigation
--- client/src/components/RIC-Widget/RelatedList.jsx
+++ client/src/components/RIC-Widget/RelatedList.jsx
@@ -105,23 +105,3 @@ const RelatedList = ({ productId }) => {
 };
 
 export default RelatedList;
-
-
-/*
-- get product Id from overview design product to then map over that list
-
-
-List Behavior
-
-- list is determined internally & related to current product view
-- # of related products finite
-- displayed in a carousel fashion
- - scrolling horizontally
- - first item on left hand side
- - arrows appear on left/right
-  - click to scroll thru list
-  - only one item per scroll click
-  - left arrow is hidden if at first item at far lest
-  - right arrow hidden if last item at far right
- - any prod not on screen should appear offscreen in carousel
-*/
