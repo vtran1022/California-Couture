1b777334f65ad745c9e87156a44252ee9b2a2a26
Vy Tran <vtran1022@Gmail.com>
2021-06-13 01:30:12 -0700

    added style loader and css loader to webpack config and installed them

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
        test: /\.(png|jpg|jpeg|gif)$/i,
        use: { loader: 'url-loader' },
      },
+     {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
     ],
   },
 };
