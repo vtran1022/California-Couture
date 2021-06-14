e74d0a76a6fb3476cd9511f77765d0a8a71fc38f
Vy Tran <vtran1022@Gmail.com>
2021-06-14 16:18:12 -0700

    cleaned up comments

--- client/src/components/RIC-Widget.jsx
+++ client/src/components/RIC-Widget.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import RelatedList from './RIC-Widget/RelatedList.jsx';
 import OutfitList from './RIC-Widget/OutfitList.jsx';
 
-// expecting this productId to be linked to current Overview Product
 const RICWidget = ({ productId, productClick }) => {
   return (
     <div id='RICWid'>
