3db71f0750c2e172a33f8f91180cb0cd2e60d40b
Julian Thong <julianzthong@gmail.com>
2021-06-14 19:59:53 -0700

    fixed typos

--- client/src/components/overview/ZoomedPreview.jsx
+++ client/src/components/overview/ZoomedPreview.jsx
@@ -5,7 +5,7 @@ function ZoomedPreview ( { view, preview }) {
   const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
   const [showMagnifier, setShowMagnifier] = useState(false);
   const magnifierHeight = 150;
-  const magnifieWidth = 150;
+  const magnifierWidth = 150;
   const zoomLevel = 2;
 
   function handleMouseMove (e) {
@@ -27,8 +27,16 @@ function ZoomedPreview ( { view, preview }) {
     setShowMagnifier(false);
   }
 
+  function checkFunctionality (src) {
+    console.log(src)
+    console.log(src.width)
+    console.log(src.height)
+  }
+
+
   return (
     <div className='preview-container'>
+      <button onClick={() => checkFunctionality(preview)}>Test me Here!!!</button>
       <img
       className='preview'
       id='zoomed-preview'
@@ -48,7 +56,7 @@ function ZoomedPreview ( { view, preview }) {
         backgroundSize: `${imgWidth * zoomLevel}px ${
           imgHeight * zoomLevel
         }px`,
-        backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
+        backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
         backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`
         }}>
       </div>
